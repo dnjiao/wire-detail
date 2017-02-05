266ada996f7b46f6c33de99b7a9f6273f21eb591
dnjiao <oscarjiao@gmail.com>
2017-02-05 10:55:47 -0600

    first commit

---
+++ src/main/java/accounting/PDFReader.java
@@ -0,0 +1,31 @@
+package accounting;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+
+public class PDFReader {
+	public static void main(String[] args) {
+		String path = "C://Users//Oscar//Desktop//Wire_Detail.pdf";
+		String text = readPDF(path);
+		System.out.println(text);
+		
+	}
+	
+	
+	public static String readPDF(String filepath) {
+		File file = new File(filepath);
+		String text = null;
+		try {
+			PDDocument document = PDDocument.load(file);
+			PDFTextStripper pdfStripper = new PDFTextStripper();
+			text = pdfStripper.getText(document);
+			
+		}  catch (IOException e) {
+			e.printStackTrace();
+		}
+		return text;
+	}
+}
